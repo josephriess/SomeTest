98e9dde96da73b0c34abda1e585d4ec677397fef
Joseph Riess <JoeRiess@joes-macbook-pro-2.local>
2016-01-19 16:27:06 -0500

    hello world

--- SomeTest/ViewController.swift
+++ SomeTest/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         print("test")
         print("booooo")
+        print("another test")
     }
 
     override func didReceiveMemoryWarning() {
